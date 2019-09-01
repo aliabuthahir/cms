f40554a4da5ec3787dbf4fc037241056eea33c3f
Ali Abu Thair <aliabuthair@gmail.com>
2019-09-01 19:37:32 +0400

    * critical bug fix for upload task component.

--- src/app/upload/upload-task/upload-task.component.ts
+++ src/app/upload/upload-task/upload-task.component.ts
@@ -64,7 +64,7 @@ export class UploadTaskComponent implements OnInit {
   }
 
   startUpload() {
-    const path = `/uploads`;
+    const path = `uploads/${Date.now()}_${this.file.name}`;
     const ref = this.storage.ref(path);
     this.task = this.storage.upload(path, this.file);
     this.percentage = this.task.percentageChanges();
